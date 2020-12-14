a40e544b9d1ca3d2f59dde48b40577fcd891dbbb
ashwini-8 <ashwinip309@gmail.com>
2020-12-14 16:07:20 +0530

    Added:UC3 Part-time employee

---
+++ EmpWageBuilderUC3.java
@@ -0,0 +1,18 @@
+public class EmpWageBuilderUC3{
+
+        public static void main(String args[]){
+                int EMP_RATE_PER_HOUR = 20;
+                int IS_FULL_TIME = 1;
+                int  IS_PART_TIME = 2;
+                double empCheck = Math.floor(Math.random() * 10) % 3;
+                int empHours = 0;
+                int empWage = 0;
+
+                if (empCheck == IS_FULL_TIME)
+                        empHours = 8;
+                else if (empCheck == IS_PART_TIME)
+                        empHours = 4;
+                empWage = empHours * EMP_RATE_PER_HOUR;
+                System.out.println("Emp wage:" + empWage);
+        }
+}
